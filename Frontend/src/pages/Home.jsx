743172e53d44cc4c91108c389b4d24c0141cc7ea
akshayyadav63 <akshay.kumar_cs22@gla.ac.in>
2025-07-18 10:10:26 +0530

    Hero Section & Ai tools part have been completed

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Navbar from '../components/NavBar'
+import Hero from '../components/Hero'
+import AiTools from '../components/AiTools'
 const Home = () => {
   return (
     <>
     <Navbar/>
+    <Hero/>
+    <AiTools/>
      
     </>
   )
