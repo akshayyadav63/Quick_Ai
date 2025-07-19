32d1ce8393b85c281b38d24c724d615d09db0503
akshayyadav63 <akshay.kumar_cs22@gla.ac.in>
2025-07-19 17:11:08 +0530

    sidebar with its component have een created

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyCreationData } from '../assets/assets';
+import { Gem, Sparkles } from 'lucide-react';
+import { Protect } from '@clerk/clerk-react';
+import CreationItem from '../components/CreationItem';
 
 const Dashboard = () => {
+  const [creation ,setCreations]=useState([]);
+  const getDashboardData=async ()=>{
+    setCreations(dummyCreationData)
+  }
+  useEffect(()=>{
+    getDashboardData()
+
+  },[])
+
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className='h-full overflow-y-scroll p-6'>
+      <div className='flex justify-start gap-4 flex-wrap'>
+    <div  className='flex justify-between items-center w-72 p-4 p-x bg-white rounded-xl border border-gray-200'>
+   <div className='text-slate-600'>
+    <p className='text-sm'>Total Creation</p>
+    <h2 className='text-xl font-semibold'>{creation.length}</h2>
+   </div>
+   <div className='w-10 h-10 rounded-lg bg-gradient-to-br form-[#3588F2] to-[#0BB0D7] text-white flex justify-center items-center'> 
+    <Sparkles className='w-5 text-white'/> </div>
+    </div>
+
+    <div  className='flex justify-between items-center w-72 p-4 p-x bg-white rounded-xl border border-gray-200'>
+   <div className='text-slate-600'>
+    <p className='text-sm'>Active Plan</p>
+    <h2 className='text-xl font-semibold'>
+      <Protect plan='premium' fallback="Free">Premium</Protect>
+    </h2>
+   </div>
+   <div className='w-10 h-10 rounded-lg bg-gradient-to-br form-[#FF61CS] to-[#9E53EE] text-white flex justify-center items-center'> 
+    <Gem className='w-5 text-white'/> </div>
+    </div>
+      </div>
+
+      <div className='space-y-3'>
+        <p className='mt-6 mb-4'>Recent Creations</p>
+        {
+          creation.map((item)=><CreationItem key={item.id} item={item}/>)
+        }
+
+      </div>
     </div>
   )
 }
